894f58e8d256d9e4a8acc7fbd1f7fbcd06eefc16
s-hashiba <shinichi.hashiba@gmail.com>
2020-07-13 16:08:11 +0900

    Add Split Method Project

---
+++ 1-SplitMethod/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _1_SplitMethod
+{
+    class Program
+    {
+        /// <summary>
+        /// Input catType and run car by catType.
+        /// </summary>
+        /// <param name="args">catType</param>
+        static void Main(string[] args)
+        {
+            writeLog("Start Process");
+
+            string carType = args[0];
+            if (!runCar(carType))
+            {
+                writeLog("No carType Found");
+            }
+
+            writeLog("End Process");
+        }
+
+        /// <summary>
+        /// Write Log In Console
+        /// </summary>
+        /// <param name="message">message to write</param>
+        static void writeLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// runnning a car by catType
+        /// </summary>
+        /// <param name="carType">car</param>
+        /// <returns>if catType found return true, else return false.</returns>
+        static bool runCar(string carType)
+        {
+            switch (carType)
+            {
+                case "FireEngine":
+                    Console.WriteLine("Peepo");
+                    return true;
+                case "Bus":
+                    Console.WriteLine("Broom");
+                    return true;
+                case "PoliceCar":
+                    Console.WriteLine("WooWoo");
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
